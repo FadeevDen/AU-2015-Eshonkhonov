be7caa21874739194f66c5508a89fa5daa9bcc4e
Azizxon <azizxon_uz@hotmail.com>
2015-09-25 17:21:26 +0300

    SignInSuccessful

---
+++ MessengerVK/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VkNet.Enums.Filters;
+namespace MessengerVK
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SignIn _signIn;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        //Закрытие окна
+        private void BnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        //Метод который позволяет перетаскивать окно 
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+        //Метод авторизации пользователя
+        private void BnSignIn_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _signIn = new SignIn(TxtLogin.Text, TxtPassword.Text, Settings.All);
+                _signIn.AuthMethod();
+                _signIn.GetProfileInfo();
+            }
+            catch(Exception)
+            {
+                 TxtBcShowMessage.Text = e.ToString();
+            }
+            if (_signIn.User!=null)
+            {
+                ShowMessageAboutSuccessfulAuth();
+            }
+            else
+            {
+                ShowMessageAboutFailedAuth();
+            }
+
+        }
+
+        public void ShowMessageAboutSuccessfulAuth()
+        {
+            TxtBcShowMessage.Text = "Вы авторизованы";
+        }
+        public void ShowMessageAboutFailedAuth()
+        {
+            TxtBcShowMessage.Text = "Авторизация не выполнена."+"\r\n"+ "Проверьте введенные данные";
+        }
+    }
+}
---
+++ MessengerVK/SignIn.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+
+namespace MessengerVK
+{
+   public class SignIn
+   {
+
+       private const int appID= 5074413;                    // ID приложения
+       private string login;                 // email или телефон
+       private string password;              // пароль для авторизации
+       private Settings scope;               //Уровень доступа
+       private VkApi api;
+       private User user;
+   
+    public User User
+        {
+            get
+            {
+                return user;
+            }
+
+            set
+            {
+                user = value;
+            }
+        }
+
+     public VkApi Api
+        {
+            get
+            {
+                return api;
+            }
+
+            set
+            {
+                api = value;
+            }
+        }
+
+        public SignIn(string login,string password,Settings scope)
+       {
+            
+            this.login = login;
+            this.password = password;
+            this.scope = scope;
+        }
+        [STAThread]
+       public void AuthMethod()
+       {
+            Api = new VkApi();
+            Api.Authorize(appID, login, password, scope);
+       }
+
+       public void GetProfileInfo()
+       {
+           user = new User();
+           if (api.UserId != null) user = api.Users.Get((long) api.UserId, ProfileFields.All, null);
+       }
+   }
+
+   
+}
